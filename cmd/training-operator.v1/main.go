ad1642063232b86b0c785fcd8fa4cb007be86a63
Antonin Stefanutti <antonin@stefanutti.fr>
2025-02-04 17:06:12 +0100

    CARRY: Enable validation webhooks

--- cmd/training-operator.v1/main.go
+++ cmd/training-operator.v1/main.go
@@ -53,7 +53,7 @@ const (
 	// EnvKubeflowNamespace is an environment variable for namespace when deployed on kubernetes
 	EnvKubeflowNamespace = "KUBEFLOW_NAMESPACE"
 
-	webhookConfigurationName = "validator.training-operator.kubeflow.org"
+	webhookConfigurationName = "kubeflow-validator.training-operator.kubeflow.org"
 )
 
 var (
